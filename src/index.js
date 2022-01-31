aa39ce0103b832c6e42e26917d5533b7bf413e30
DavidRaj023 <benjamindavid023@gmail.com>
2022-01-31 22:13:41 +0530

    Added simple front end

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const path = require('path');
 
 const songsRouter = require('./routes/songRoutes');
@@ -6,9 +7,23 @@ const songsRouter = require('./routes/songRoutes');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.json());
 songsRouter(app);
 
+const filePath = path.join(__dirname, '../resources/index.html');
+
+app.get("/", (req, res) => {
+  res.sendFile(filePath);
+});
+
+// app.post("/", (req, res) => {
+//     console.log(req.body);
+// })
 app.listen(port, () => {
     console.log('Server up and running on port: ' + port);
 })
+
+
+
--- src/routes/songRoutes.js
+++ src/routes/songRoutes.js
@@ -5,7 +5,8 @@ const upload = require('../utils/upload');;
 
 let routes = (app) => {
     try {
-        router.post('/api/v1/addNewSong' , controller.addSong);
+        //router.post('/api/v1/addNewSong' , controller.addSong);
+        router.post('/' , controller.addSong);
         router.post('/api/v1/addFromExcel', upload.single('file'), controller.insertFromExcel);
         router.get('/api/v1/songs/export', controller.exportExcel);
         router.post('/api/v1/songs/export/pdf', controller.exportPDF);
@@ -15,7 +16,6 @@ let routes = (app) => {
     } catch (error) {
         console.log(error);
     }
-
 };
 
 module.exports = routes;
